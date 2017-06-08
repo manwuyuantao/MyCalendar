9f5479fff28d0b5d07d9d901b758fb258dc9fc51
manwuyuantao <manwuyuantao@qq.com>
2017-06-08 15:57:54 +0800

    init proj

---
+++ app/src/main/java/com/wuyuantao/mycalendar/MyCalendar.java
@@ -0,0 +1,25 @@
+package com.wuyuantao.mycalendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by wuyuantao on 2017/6/8.
+ */
+
+public class MyCalendar extends LinearLayout {
+
+    public MyCalendar(Context context) {
+        super(context);
+    }
+
+    public MyCalendar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
